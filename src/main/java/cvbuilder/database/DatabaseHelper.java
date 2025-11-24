4e2358c028e479163fce110daf0c3cf611fd3b93
Shomik Shahriar <151670863+Hapi-Guy@users.noreply.github.com>
2025-11-25 05:46:11 +0600

    Final commit extended so fixes

--- src/main/java/cvbuilder/database/DatabaseHelper.java
+++ src/main/java/cvbuilder/database/DatabaseHelper.java
@@ -1,12 +1,18 @@
 package cvbuilder.database;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import cvbuilder.model.CV;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import cvbuilder.model.CV;
 
 /**
  * Simple SQLite database helper for CV storage
